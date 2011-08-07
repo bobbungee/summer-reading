2c64196cba930735d1e06338115fc3dde4d4af06
Rishi Shah <bobbungee@gmail.com>
2011-08-06 22:45:52 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Summerreading::Application.config.secret_token = '33dacfad21ba9716bb8cce804a47923a978fcf46611ef7592c38f139b27d53dbe2f89c2ef385918cc7011e0ebc0ca2ac1fe8e43065754713e3d579cd5ba7617c'
