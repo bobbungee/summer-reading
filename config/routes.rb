3760f026386ce6af918a58fed16fd6b14a39df2a
Rishi Shah <bobbungee@gmail.com>
2011-08-06 23:39:25 -0500

    Welcome scaffolding

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Summerreading::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
+  
+  root :to => 'welcome#index'
 
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
